247bd051f7eceac66a26065280041e16e083bf25
Joshua <nickwulfe@gmail.com>
2018-05-16 14:46:48 -0400

    updated comic book model

--- src/ComicBookGallery/Models/ComicBooks.cs
+++ src/ComicBookGallery/Models/ComicBooks.cs
@@ -13,5 +13,24 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artists[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayTest
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ","-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }

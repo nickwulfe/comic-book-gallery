10f66b32e9da300d4b45cfc425136c9d0c82ac2f
Joshua <nickwulfe@gmail.com>
2018-05-22 12:33:12 -0400

    updated comic books controller to use comic book repository

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using ComicBookGallery.Models;
 
+
+
 namespace ComicBookGallery.Data
 {
     public class ComicBookRepository
@@ -12,6 +14,7 @@ namespace ComicBookGallery.Data
     {
     new ComicBook()
     {
+        ID = 1,
         SeriesTitle = "The Amazing Spider-Man",
         IssueNumber = 700,
         DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
@@ -27,6 +30,7 @@ namespace ComicBookGallery.Data
     },
     new ComicBook()
     {
+        ID = 2,
         SeriesTitle = "The Amazing Spider-Man",
         IssueNumber = 657,
         DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -42,6 +46,7 @@ namespace ComicBookGallery.Data
     },
     new ComicBook()
     {
+        ID = 3,
         SeriesTitle = "Bone",
         IssueNumber = 50,
         DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
@@ -62,7 +67,7 @@ namespace ComicBookGallery.Data
 
             foreach (var comicBook in _comicBooks)
             {
-                if(comicBook.Id == id)
+                if(comicBook.ID == id)
                 {
                     comicBookToReturn = comicBook;
 
